20cc4d7fb3ab8c5b23335d515313ffefa0c02543
송호정 <>
2024-08-27 16:48:29 +0900

    feat: ProjectBS 프로젝트 파일 추가

---
+++ Source/ProjectBS/ProjectBS.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectBS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectBS, "ProjectBS" );
+
+DEFINE_LOG_CATEGORY(LogProjectBS)
+ 
